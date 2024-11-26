1865d4e3cb6bc1c25d674913c7db5b8c895f8918
Rolernock <rolernockgoines@gmail.com>
2024-11-26 20:16:12 +0300

    changed database from local to remote

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const ConnectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_COMPASS)
+    const conn = await mongoose.connect(process.env.MONGO_ATLAS)
     console.log(`MongoDB connected on: ${conn.connection.host}`)
   } catch (err) {
     console.error(err.message)
